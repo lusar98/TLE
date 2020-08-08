06f29b3ce029eebc5cc0aa2b26a9133ad392a49c
rk_98 <iamrasul786@gmail.com>
2020-08-08 17:30:00 +0530

    added daemon script

---
+++ daemon.py
@@ -0,0 +1,10 @@
+import time
+import os
+
+var = 1;
+while var == 1:
+	print("BOT will start in a few seconds...")
+	time.sleep(5)
+	print("BOT will start now")
+	os.system("ls")
+	print("BOT has stopped... :(")
